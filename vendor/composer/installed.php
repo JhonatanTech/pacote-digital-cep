2e1f81c9a09695270e210691cfe479c96505ccee
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2021-07-25 17:11:53 -0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'jhonatan/php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jhonatan/php' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'wead/digital-cep' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wead/digital-cep',
+            'aliases' => array(),
+            'reference' => '63b5624635c0c1e0f2020f672dcfd235dd9834ff',
+            'dev_requirement' => false,
+        ),
+    ),
+);
